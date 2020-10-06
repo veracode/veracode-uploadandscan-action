0bb8d79ee63c5cbff44aa91cfcb33666cbe1bed8
julz0815 <j.totzek@gmail.com>
2020-10-06 12:53:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,4 +102,5 @@ javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/w
 
 curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
 chmod 777 runJava.sh
+more runJava.sh
 ./runJava.sh
