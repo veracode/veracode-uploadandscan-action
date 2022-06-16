23095b2e06ea9e758d5e7bbc6e7d39cb9f4541f0
Sina Taghizadeh <sinat101@users.noreply.github.com>
2022-06-16 17:46:33 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -232,7 +232,7 @@ then
 fi
 
 
-echo "        -createprofile \"$createprofile\"" >> runJava.sh
+echo "        -createprofile \"$createprofile\" \\" >> runJava.sh
 
 if [ "$javawrapperversion" ]
 then 
@@ -245,7 +245,7 @@ echo "javawrapperversion: $javawrapperversion"
 
 if [ "$debug" ]
 then
-    echo "        -debug \"$debug\" \\" >> runJava.sh
+    echo "        -debug \"$debug\"" >> runJava.sh
 fi
 
 curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
